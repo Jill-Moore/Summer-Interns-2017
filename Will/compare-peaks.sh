84565707cd40af4a82d4d978d6feca6f97c94fcb
Will <willzhang100@gmail.com>
2017-08-17 12:57:12 -0400

    Updated compare peaks script and added test peak files

--- Will/compare-peaks.sh
+++ Will/compare-peaks.sh
@@ -4,18 +4,29 @@
 
 if [ $# -eq 0 ]; then
     echo "Tool:    compare-peaks"
-    echo "Summary: Compare the percent overlap between two histone mark datasets in the same cell type for the same mark."
-    echo "Usage:   ./compare-peaks.sh peaks1.bed peaks2.bed"
+    echo "Summary: Compare the percent overlap between two histone mark datasets in the same cell type for the same mark. Generate scatterplots that compare signals over each peak."
+    echo "Usage:   ./compare-peaks.sh peaks1.bed peaks2.bed [signal1.bigwig] [signal2.bigwig]"
     exit 1
 fi
+
+### Compute number of peaks and intersection ###
 
 peaks1=$1
 peaks2=$2
 
-echo "% of " $peaks1 " that overlaps with " $peaks2 ":"
-bedtools intersect -u -a $peaks1 -b $peaks2 | wc -l
+numpeaks=$( cat $peaks1 | wc -l )
+overlap=$( bedtools intersect -u -a $peaks1 -b $peaks2 | wc -l )
+percent=$( echo "scale=2; 100 * $overlap / $numpeaks" | bc )
+echo $peaks1
+echo "  total peaks: "$numpeaks
+echo "  percent overlap: "$percent" %"
 
-echo "% of " $peaks2 " that overlaps with " $peaks1 ":"
-bedtools intersect -u -a $peaks2 -b $peaks1 | wc -l
+numpeaks=$( cat $peaks2 | wc -l )
+overlap=$( bedtools intersect -u -a $peaks2 -b $peaks1 | wc -l )
+percent=$( echo "scale=2; 100 * $overlap / $numpeaks" | bc )
+echo $peaks2
+echo "  total peaks: "$numpeaks
+echo "  percent overlap: "$percent" %"
 
+### Generate scatterplots comparing signals between each lab over each peak ###
 
