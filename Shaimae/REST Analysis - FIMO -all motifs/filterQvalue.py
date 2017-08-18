be2b6e95afe5d930d699d8e83a7b03edf8d02882
orbitalse <31048432+orbitalse@users.noreply.github.com>
2017-08-18 11:36:29 -0400

    Apply threshold of q-value < 0.05

---
+++ Shaimae/REST Analysis - FIMO -all motifs/filterQvalue.py
@@ -0,0 +1,33 @@
+# Author: Shaimae Elhajjajy
+# Script to find all of the lines in fimo.txt that have a q-value less than 0.05
+
+# Open the data files
+with open('listOfMotifs_all.txt') as f:
+        content = f.readlines()
+content=[x.strip() for x in content]
+listOfMotifs = [str(value) for value in content]
+
+with open('listOfPeaks_all.txt') as f:
+       content = f.readlines()
+content=[x.strip() for x in content]
+listOfPeaks = [str(value) for value in content]
+
+with open('qvalues_all.txt') as f:
+       content = f.readlines()
+content=[x.strip() for x in content]
+qvalues = [str(value) for value in content]
+
+# Cycle through the file and filter accordingly
+
+for i in range(0, len(qvalues)):
+	# Set constraint for q-value less than 0.05
+	if float(qvalues[i]) < 0.05:
+		# Write results to a file
+        	f= open("fimo_filtered.txt", "a+")
+                f.write(listOfMotifs[i])
+                f.write("\t")
+		f.write(listOfPeaks[i])
+                f.write("\t")
+                f.write(qvalues[i])
+                f.write("\n")
+                f.close()
