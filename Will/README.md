c141508c0d0a9221c728641f7c0ec74bd0fd9cc1
Will <willzhang100@gmail.com>
2017-08-23 13:56:04 -0400

    Added writeup.

--- Will/README.md
+++ Will/README.md
@@ -1,1 +1,2 @@
 # Will
+[Link to summer 2017 writeup] (https://docs.google.com/document/d/1LQ1XsPjsLwSd2wpWIcTwI6zAeM62kAW3FRR_URx9Dos/edit) 
--- Will/cistrome-overlap/top-overlap.R
+++ Will/cistrome-overlap/top-overlap.R
@@ -8,12 +8,14 @@ table<-NULL
 
 if(length(args) == 0) {
   cat("Tool:    top-overlap")
-  cat("Summary: create bargraph of the % overlap of the top (by count) histone mark/TF.")
-  cat("Usage:   Rscript top-overlap.R table")
-  stop("Incorrect number of arguments.")
+  cat("Summary: create bargraph of the % overlap of the top (by count) histone mark/TF.\n")
+  cat("Usage:   Rscript top-overlap.R table\n")
+  stop("Incorrect number of arguments.\n")
 } else if(length(args) == 1) {
-  table<-read.table(args[1])
+  table<-read.table(args[1], header = TRUE)
+  cat(args[1])
 } else {
   stop("Incorrect number of arguments.")
 }
 
+p <- ggplot(table, aes())
